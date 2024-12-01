ef84a00bd4c69c0cd19dc40fb0b2d9657ed889ce
sumomo015 <138793740+sumomo015@users.noreply.github.com>
2024-12-02 05:34:12 +0900

    Add layout components for header and footer with responsive design

---
+++ components/Layouts/LayoutsFooter.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const now = useNow()
+const year = computed(() => now.value.getFullYear())
+</script>
+
+<template>
+  <footer class="text-center text-sm text-gray-500 dark:text-gray-400">
+    <p class="mb-2">
+      Made with 💚 in Tokyo<br>
+      using Vue, Nuxt, and UnoCSS.
+    </p>
+    <p>© {{ year }} - sumomo015.dev</p>
+  </footer>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+const isDarkMode = computed(() => colorMode.value === 'dark')
+
+function toggleColorScheme() {
+  colorMode.preference = isDarkMode.value ? 'light' : 'dark'
+}
+</script>
+
+<template>
+  <div class="h-full flex flex-col">
+    <LayoutsHeader
+      :is-dark-mode="isDarkMode"
+      @toggle-color-scheme="toggleColorScheme"
+    />
+
+    <main class="flex-grow-1">
+      <slot />
+    </main>
+
+    <LayoutsFooter class="py-10 sm:py-16" />
+  </div>
+</template>
