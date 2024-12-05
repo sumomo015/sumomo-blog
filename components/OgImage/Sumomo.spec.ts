b9d4df122143a4d1f41e61cda2f6d911f8fb409d
sumomo015 <138793740+sumomo015@users.noreply.github.com>
2024-12-06 01:12:46 +0900

    fix: OgImageSumomoのインポートパスを修正

--- components/OgImage/Sumomo.spec.ts
+++ components/OgImage/Sumomo.spec.ts
@@ -3,7 +3,7 @@
 import { describe, it, expect } from 'vitest'
 import { mountSuspended } from '@nuxt/test-utils/runtime'
 
-import { OgImageSumomo } from '#components'
+import OgImageSumomo from './Sumomo.vue'
 
 describe('OgImageSumomo', () => {
   const selectorTitle = '[data-testid="title"]'
