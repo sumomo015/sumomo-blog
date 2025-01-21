a513c37ac4a1ee3188b761f4631b42ed6d990d94
sumomo015 <138793740+sumomo015@users.noreply.github.com>
2025-01-22 01:43:31 +0900

    test: useAnchorLinksのテストを追加し、見出しがundefinedの場合の動作を確認

--- composables/useAnchorLinks.ts
+++ composables/useAnchorLinks.ts
@@ -13,18 +13,21 @@ export function useAnchorLinks() {
   const { headings } = useRuntimeConfig().public.mdc
 
   const generateInfo = computed<GeratateInfo>(() => {
-    if (typeof headings.anchorLinks === 'boolean' && headings.anchorLinks === true) {
+    if (!headings)
+      return { h1: false, h2: false, h3: false, h4: false, h5: false, h6: false }
+
+    if (typeof headings.anchorLinks === 'boolean' && headings.anchorLinks) {
       return { h1: true, h2: true, h3: true, h4: true, h5: true, h6: true }
     }
 
     if (typeof headings.anchorLinks === 'object') {
       return {
-        h1: headings.anchorLinks.h1 || false,
-        h2: headings.anchorLinks.h2 || false,
-        h3: headings.anchorLinks.h3 || false,
-        h4: headings.anchorLinks.h4 || false,
-        h5: headings.anchorLinks.h5 || false,
-        h6: headings.anchorLinks.h6 || false,
+        h1: headings.anchorLinks.h1 ?? false,
+        h2: headings.anchorLinks.h2 ?? false,
+        h3: headings.anchorLinks.h3 ?? false,
+        h4: headings.anchorLinks.h4 ?? false,
+        h5: headings.anchorLinks.h5 ?? false,
+        h6: headings.anchorLinks.h6 ?? false,
       }
     }
 
