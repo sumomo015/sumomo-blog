3f4e7bce1b5a58c344fcb3334c87981078d357dd
sumomo015 <138793740+sumomo015@users.noreply.github.com>
2025-03-27 23:29:42 +0900

    chore: Set nitro preset to static in nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -68,6 +68,7 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2024-11-01',
   nitro: {
+    preset: 'static',
     output: {
       publicDir: '{{ output.dir }}/static{{ baseURL }}',
     },
